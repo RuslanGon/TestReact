630337722006e8925797fd06b338f89743bd6db5
RuslanGon <ruslan441006@gmail.com>
2024-09-15 10:53:13 +0300

    wer

--- src/components/MailboxFilter/MailboxFilter.jsx
+++ src/components/MailboxFilter/MailboxFilter.jsx
@@ -1,12 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectFilter } from "../../redux/mailbox/selectors.js";
+import { setFilter } from "../../redux/mailbox/mailboxReducer.js";
 
+const MailboxFilter = () => {
+ const dispatch = useDispatch()
+ const filter = useSelector(selectFilter)
 
-const MailboxFilter = ({filter, onChangeFilter}) => {
+ const onChangeFilter = (event) => {
+
+    dispatch(setFilter(event.target.value))
+
+  }
+
   return (
-<section>
-        <h3>Search user by name or emaile</h3>
-        <input  type="text" placeholder='🔍  search...' value={filter} onChange={onChangeFilter} />
-      </section>
-  )
-}
+    <section>
+      <h3>Search user by name or emaile</h3>
+      <input
+        type="text"
+        placeholder="🔍  search..."
+        value={filter}
+        onChange={onChangeFilter}
+      />
+    </section>
+  );
+};
 
-export default MailboxFilter
+export default MailboxFilter;
