f02945bf99a92066358fbe4ad8799ea3575b3f3e
RuslanGon <ruslan441006@gmail.com>
2024-09-15 12:59:14 +0300

    wer

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,53 @@
 
-const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-export default LoginPage
+
+const LoginSchema = Yup.object({
+  email: Yup.string().email("Invalid email format").required("email is required"),
+  password: Yup.string().required("password is required"),
+});
+
+
+const FORM_INITIAL_VALUES = {
+  email: "",
+  password: ''
+};
+
+const LoginPage = () => {
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={FORM_INITIAL_VALUES}
+      onSubmit={handleSubmit}
+      validationSchema={LoginSchema}
+    >
+      <Form>
+        <h2>Login user</h2>
+        <br />
+        <label>
+          <span>email:</span>
+          <br />
+          <Field type="email" name="email" placeholder="email" />
+          <ErrorMessage name="email" component="span" />
+          <br />
+        </label>
+       <label>
+          <span>password:</span>
+          <br />
+          <Field type="text" name="password" placeholder="password" />
+          <ErrorMessage name="password" component="span" />
+          <br />
+        </label>
+      <br />
+      <button type="submit">Login user 👱</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default LoginPage;
