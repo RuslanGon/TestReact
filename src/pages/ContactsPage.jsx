025de105cd771192c4bcab35252cc4825756bb48
RuslanGon <ruslan441006@gmail.com>
2024-09-15 18:48:55 +0300

    wer

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader/Loader.jsx";
+import { Error } from "../components/Error/Error.jsx";
 
 const ContactsPage = () => {
-  return (
-    <div>ContactsPage</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-export default ContactsPage
+  const loadData = () => {
+    try {
+      setIsLoading(true);
+      // Имитация загрузки данных
+      setTimeout(() => {
+        setIsLoading(false); // Успешная загрузка
+      }, 1000); // Задержка в 1 секунду для примера
+    } catch (error) {
+      setIsError(true);
+      console.log(error);
+    }
+  };
+
+  // Вызов функции loadData только при монтировании компонента
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div>
+      {isLoading && <Loader />}
+      {isError && <Error />}
+      <h3>Contacts</h3>
+    </div>
+  );
+};
+
+export default ContactsPage;
