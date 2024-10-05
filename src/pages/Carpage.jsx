1b3ef864999db7902b223ab9c63a4379255f5f10
RuslanGon <ruslan441006@gmail.com>
2024-10-05 11:37:47 +0300

    wer

--- src/pages/Carpage.jsx
+++ src/pages/Carpage.jsx
@@ -1,16 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Loader from "../components/Loader/Loader.jsx";
+import { Error } from "../components/Error/Error.jsx";
 
 const CarPage = () => {
 
 const [cars, setCars] = useState(null)
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(false)
 
 useEffect(() => {
   async function fetchCars() {
+    try {
+      setLoading(true)
     const response = await axios.get('https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers')
-    // console.log(data);
-    console.log(response.data);
     setCars(response.data.items)
+    } catch (error) {
+      console.log(error);
+      setError(true)
+    } finally {
+      setLoading(false)
+    }
   }
   fetchCars();
 }, []);
@@ -18,6 +28,8 @@ useEffect(() => {
   return (
     <div>
       <h1>Campers</h1>
+      {loading && <Loader/>}
+      {error && <Error />}
       <ul>
      {Array.isArray(cars) && cars.map(car => {
       return (<li key={car.id}>
