3909cec156b69ed8ae8e1f14e6b757d65364a892
RuslanGon <ruslan441006@gmail.com>
2024-10-05 11:19:08 +0300

    wer

--- src/pages/Carpage.jsx
+++ src/pages/Carpage.jsx
@@ -7,9 +7,10 @@ const [cars, setCars] = useState(null)
 
 useEffect(() => {
   async function fetchCars() {
-    const {data} = await axios.get('https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers')
-    console.log(data);
-    setCars(data.items)
+    const response = await axios.get('https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers')
+    // console.log(data);
+    console.log(response.data);
+    setCars(response.data.items)
   }
   fetchCars();
 }, []);
