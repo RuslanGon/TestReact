65926dd53aa9b41d2e89f0dadaa868626b227b92
RuslanGon <ruslan441006@gmail.com>
2024-10-05 10:37:32 +0300

    wer

--- src/pages/Carpage.jsx
+++ src/pages/Carpage.jsx
@@ -1,9 +1,33 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const CarPage = () => {
+
+const [car, setCar] = useState(null)
+
+useEffect(() => {
+  async function fetchCars() {
+    const {data} = await axios.get('https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers')
+    console.log(data);
+  }
+  fetchCars();
+}, []);
+
   return (
-    <div>Carpage</div>
-  )
-}
+    <div>
+      <h1>Campers</h1>
+      <ul>
+        <li>
+          <img width={550} height={250} src="" alt="" />
+          <h2>Name</h2>
+          <h3>Price</h3>
+          <p>Rating</p>
+          <p>Location</p>
+          <p>Form</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default CarPage
