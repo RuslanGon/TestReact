1749db1c78b41490fa1457ed1f44bbc07a2019c3
RuslanGon <ruslan441006@gmail.com>
2024-10-05 14:41:48 +0300

    wer

--- src/redux/cars/selectors.js
+++ src/redux/cars/selectors.js
@@ -1,4 +1,5 @@
 export const selectCar = (state) => state.cars.car
+
 export const selectLoading = (state) => state.cars.loading
 export const selectError = (state) => state.cars.error
 export const selectQuery = (state) => state.cars.query
