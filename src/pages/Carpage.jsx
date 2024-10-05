86b3eefb6218d9f3f16bc348aa106b99e43e2aa4
RuslanGon <ruslan441006@gmail.com>
2024-10-05 11:08:50 +0300

    wer

--- src/pages/Carpage.jsx
+++ src/pages/Carpage.jsx
@@ -20,7 +20,7 @@ useEffect(() => {
       <ul>
      {Array.isArray(cars) && cars.map(car => {
       return (<li key={car.id}>
-        <img width={550} height={250} src="" alt={car.name} />
+        <img width={550} height={250} src={car.gallery[0]?.original} alt={car.name} />
         <h2>Name: {car.name}</h2>
         <h3>Price: {car.price}</h3>
         <p>Rating: {car.rating}</p>
