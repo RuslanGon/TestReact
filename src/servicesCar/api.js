470913266b00ef988c4ec70fc84565a3b2770020
RuslanGon <ruslan441006@gmail.com>
2024-10-05 13:09:26 +0300

    wer

--- src/servicesCar/api.js
+++ src/servicesCar/api.js
@@ -6,6 +6,8 @@ export const requestCars = async () => {
 }
 
 export const requestCarsByQuery = async (query = '') => {
-    const { data } = await axios.get(`https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers/search?q=${query}`);
-    return data
+    const { data } = await axios.get(
+        `https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers?search=${query}`
+      );
+      return data;
 }
