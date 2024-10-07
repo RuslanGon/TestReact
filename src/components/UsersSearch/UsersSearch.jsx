589b4c960e1b8e857a0625d51aaad0bf743ebdd7
RuslanGon <ruslan441006@gmail.com>
2024-10-07 16:19:53 +0300

    wer

--- src/components/UsersSearch/UsersSearch.jsx
+++ src/components/UsersSearch/UsersSearch.jsx
@@ -2,22 +2,19 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 
-const mailBoxSchema = Yup.object({
-  userEmail: Yup.string().email("Invalid email format").required("Email is required"),
-  userName: Yup.string().required("Name is required"),
-  favColor: Yup.string().required("favColor is required").oneOf(['red', 'green', 'blue'])
+const usersSearchSchema = Yup.object({
+  name: Yup.string().required("Name is required"),
 });
 
 
 const FORM_INITIAL_VALUES = {
-  userEmail: "",
-  userName: "",
-  favColor: ''
+  name: "",
+ 
 };
 
-const UsersSearch = ({ onAddUsers }) => {
+const UsersSearch = () => {
   const handleSubmit = (values, actions) => {
-    onAddUsers(values);
+    console.log(values);
     actions.resetForm();
   };
 
@@ -25,42 +22,18 @@ const UsersSearch = ({ onAddUsers }) => {
     <Formik
       initialValues={FORM_INITIAL_VALUES}
       onSubmit={handleSubmit}
-      validationSchema={mailBoxSchema}
+      validationSchema={usersSearchSchema}
     >
       <Form>
-        <h2>Add new MailBox user</h2>
+        <h2>Search user</h2>
         <label>
           <span>User email:</span>
           <br />
-          <Field type="email" name="userEmail" placeholder="email" />
-          <ErrorMessage name="userEmail" component="span" />
+          <Field type="text" name="name" placeholder="search" />
+          <ErrorMessage name="name" component="span" />
         </label>
         <br />
-        <label>
-          <span>User name:</span>
-          <br />
-          <Field type="text" name="userName" placeholder="name" />
-          <ErrorMessage name="userName" component="span" />
-          <br />
-        </label>
-        <br />
-        <span>favourite color</span>
-        <br />
-        <label>
-          <Field type="radio" name="favColor" value="red" />
-          <span>Red:</span>
-        </label>
-        <label>
-          <Field type="radio" name="favColor" value="green" />
-          <span>Green:</span>
-        </label>
-        <label>
-          <Field type="radio" name="favColor" value="blue" />
-          <span>Blue:</span>
-          <ErrorMessage component="p" name="favColor" />
-        </label>
-      <br />
-      <button type="submit">Create new user 👱</button>
+      <button type="submit">Search 👱</button>
       </Form>
     </Formik>
   );
