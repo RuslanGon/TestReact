348ffd79a394ad86fea1292dc822eb04f63a1eeb
RuslanGon <ruslan441006@gmail.com>
2024-10-07 18:24:41 +0300

    wer

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -43,6 +43,54 @@ const UserDetails = () => {
           <p>Age: {user.age}</p>
           <p>Gender: {user.gender}</p>
           <p>Phone: {user.phone}</p>
+          <p>Username: {user.username}</p>
+          <p>Birth Date: {user.birthDate}</p>
+          <p>Blood Group: {user.bloodGroup}</p>
+          <p>Height: {user.height}</p>
+          <p>Weight: {user.weight}</p>
+          <p>Eye Color: {user.eyeColor}</p>
+          <h5>Hair:</h5>
+          {user.hair ? (
+            <>
+              <p>Color: {user.hair.color}</p>
+              <p>Type: {user.hair.type}</p>
+            </>
+          ) : (
+            <p>Hair information not available</p>
+          )}
+          <h5>Address:</h5>
+          {user.address ? (
+            <p>
+              {user.address.address}, {user.address.city}, {user.address.state} {user.address.postalCode}, {user.address.country}
+            </p>
+          ) : (
+            <p>Address information not available</p>
+          )}
+          <h5>University: {user.university}</h5>
+          <h5>Company:</h5>
+          {user.company ? (
+            <>
+              <p>Name: {user.company.name}</p>
+              <p>Title: {user.company.title}</p>
+              <p>Department: {user.company.department}</p>
+              <p>
+                Company Address: {user.company.address.address}, {user.company.address.city}, {user.company.address.state} {user.company.address.postalCode}, {user.company.address.country}
+              </p>
+            </>
+          ) : (
+            <p>Company information not available</p>
+          )}
+          <h5>Role: {user.role}</h5>
+          <h5>Bank Information:</h5>
+          {user.bank ? (
+            <>
+              <p>Card Number: {user.bank.cardNumber}</p>
+              <p>Card Type: {user.bank.cardType}</p>
+              <p>Card Expiry: {user.bank.cardExpire}</p>
+            </>
+          ) : (
+            <p>Bank information not available</p>
+          )}
           <Link to={"/users"}>Go back</Link>
         </div>
       )}
