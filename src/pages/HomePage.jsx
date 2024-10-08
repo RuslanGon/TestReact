8383e92878376b21fcfa8243faac66f80f52af93
RuslanGon <ruslan441006@gmail.com>
2024-10-08 12:21:24 +0300

    wer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
-
+import css from '../pages/HomePage.module.css'
 
 const HomePage = () => {
     return (
-      <div style={{ textAlign: 'center', padding: '20px' }}>
+      <div className={css.maindiv }>
+        <div className={css.text}>
         <h1>Welcome to the Home Page 📃</h1>
         <p>This is the main landing page of our application. Here you can find links to various sections of our website.</p>
+        </div>
       </div>
     );
   }
