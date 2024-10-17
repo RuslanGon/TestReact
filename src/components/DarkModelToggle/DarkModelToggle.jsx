1def7e6e037ae8986c2c4f56e07a08966d779f50
RuslanGon <ruslan441006@gmail.com>
2024-10-17 16:33:30 +0300

    wer

--- src/components/DarkModelToggle/DarkModelToggle.jsx
+++ src/components/DarkModelToggle/DarkModelToggle.jsx
@@ -1,11 +1,12 @@
 import css from '../DarkModelToggle/DarkModelToggle.module.css'
 
 const DarkModelToggle = () => {
+    const mode = 'dark'
   return (
     <div className={css.container} >
     <div className={css.icon}>🌙</div>
     <div className={css.icon}>🔆</div>
-    <div className={css.ball}></div>
+    <div className={css.ball} style={mode === "light" ? {left: "2px"} : {right: "2px"}}></div>
 </div>
   )
 }
