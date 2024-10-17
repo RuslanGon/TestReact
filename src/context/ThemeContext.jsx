6fff4497d558f47fabec92c3c62b95a0ce59421d
RuslanGon <ruslan441006@gmail.com>
2024-10-17 16:47:36 +0300

    wer

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [mode, setMode] = useState("dark");
+
+  const toggle = () => setMode((prev) => (prev === "dark" ? "light" : "dark"));
+
+  return(
+    <ThemeContext.Provider value={{toggle, mode}}>
+        <div>{children}</div>
+    </ThemeContext.Provider>
+  )
+};
